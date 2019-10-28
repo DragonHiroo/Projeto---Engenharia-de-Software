d0627ce67f629d0c4562faabc8aff0b5275082dc
Guilherme Rodrigues <guilherme.rodriguesft@gmai.com>
2019-10-28 12:09:42 -0300

    Adcionando cores e cadastro de cirurgia

--- Projeto/cadastro.php
+++ Projeto/cadastro.php
@@ -58,7 +58,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="cadastro.php">
+              <a class="nav-link" href="#">
                 <!-- span data-feather="file-text"></span-->
                 Cadastrar cirurgia
               </a>
@@ -113,137 +113,61 @@
           </button-->
           </div>
         </div>
-
-        <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-        <h2>Cirurgias do Mês</h2>
-        <div class="table-responsive">
-          <table class="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1,001</td>
-                <td>Lorem</td>
-                <td>ipsum</td>
-                <td>dolor</td>
-                <td>sit</td>
-              </tr>
-              <tr>
-                <td>1,002</td>
-                <td>amet</td>
-                <td>consectetur</td>
-                <td>adipiscing</td>
-                <td>elit</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>Integer</td>
-                <td>nec</td>
-                <td>odio</td>
-                <td>Praesent</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>libero</td>
-                <td>Sed</td>
-                <td>cursus</td>
-                <td>ante</td>
-              </tr>
-              <tr>
-                <td>1,004</td>
-                <td>dapibus</td>
-                <td>diam</td>
-                <td>Sed</td>
-                <td>nisi</td>
-              </tr>
-              <tr>
-                <td>1,005</td>
-                <td>Nulla</td>
-                <td>quis</td>
-                <td>sem</td>
-                <td>at</td>
-              </tr>
-              <tr>
-                <td>1,006</td>
-                <td>nibh</td>
-                <td>elementum</td>
-                <td>imperdiet</td>
-                <td>Duis</td>
-              </tr>
-              <tr>
-                <td>1,007</td>
-                <td>sagittis</td>
-                <td>ipsum</td>
-                <td>Praesent</td>
-                <td>mauris</td>
-              </tr>
-              <tr>
-                <td>1,008</td>
-                <td>Fusce</td>
-                <td>nec</td>
-                <td>tellus</td>
-                <td>sed</td>
-              </tr>
-              <tr>
-                <td>1,009</td>
-                <td>augue</td>
-                <td>semper</td>
-                <td>porta</td>
-                <td>Mauris</td>
-              </tr>
-              <tr>
-                <td>1,010</td>
-                <td>massa</td>
-                <td>Vestibulum</td>
-                <td>lacinia</td>
-                <td>arcu</td>
-              </tr>
-              <tr>
-                <td>1,011</td>
-                <td>eget</td>
-                <td>nulla</td>
-                <td>Class</td>
-                <td>aptent</td>
-              </tr>
-              <tr>
-                <td>1,012</td>
-                <td>taciti</td>
-                <td>sociosqu</td>
-                <td>ad</td>
-                <td>litora</td>
-              </tr>
-              <tr>
-                <td>1,013</td>
-                <td>torquent</td>
-                <td>per</td>
-                <td>conubia</td>
-                <td>nostra</td>
-              </tr>
-              <tr>
-                <td>1,014</td>
-                <td>per</td>
-                <td>inceptos</td>
-                <td>himenaeos</td>
-                <td>Curabitur</td>
-              </tr>
-              <tr>
-                <td>1,015</td>
-                <td>sodales</td>
-                <td>ligula</td>
-                <td>in</td>
-                <td>libero</td>
-              </tr>
-            </tbody>
-          </table>
+        
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <form action="">
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="exampleInputEmail1">Médico Responsável</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="CRM">
+                        <small id="emailHelp" class="form-text text-muted">Por favor, digite o código CRM</small>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="exampleInputPassword1">Enferméiro Responsavel</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="COREM">
+                        <small id="emailHelp" class="form-text text-muted">Por favor, digite o código COREM</small>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="exampleInputPassword1">Paciente</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Código do Paciente">
+                        <small id="emailHelp" class="form-text text-muted">Por favor, digite o código do Pacietne</small>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="exampleInputPassword1">Especialidade</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="nome da Especialidade">
+                        <small id="emailHelp" class="form-text text-muted">Por favor, digite o nome da Especialidade</small>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="exampleInputPassword1">Data</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite a Data">
+                        <small id="emailHelp" class="form-text text-muted">Por favor, digite a Data</small>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="exampleInputPassword1">Hora de Inicio</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Hora de Incio">
+                        <small id="emailHelp" class="form-text text-muted">Por favor, digite a Hora de Inicio</small>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="exampleInputPassword1">Hora de Término</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Hora de Termino">
+                        <small id="emailHelp" class="form-text text-muted">Por favor, digite a Hora de Termino</small>
+                    </div>
+                </div>
+                <button type="submit" class="btn text-light" style="background-color: #41CCA5;">Enviar</button>
+            </form>
         </div>
+
+
+
+
+        <!--canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas-->
+        
+
+        
       </main>
     </div>
   </div>
