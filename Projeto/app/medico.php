5383cf567e579ff3a7d5f4f3c2c99d89973ff51c
walo2 <42192595+walo2@users.noreply.github.com>
2019-11-17 01:26:32 -0300

    Implementação da busca por médico usando CRN e alterações no front

--- Projeto/app/medico.php
+++ Projeto/app/medico.php
@@ -9,15 +9,10 @@ class medico extends Model
 {
     public static function busca_crm($info) {
         $resultado = DB::select('SELECT crm, nome, nome_especialidade, data, corem_enfermeiro, cpf_paciente, hora_inicio, duracao from medico, cirurgia WHERE medico.crm = cirurgia.crm_medico AND medico.crm = :crm ORDER BY cirurgia.data DESC', ['crm' => $info]);
-        if ($resultado) {
-            return 'enc';
-        }
-        else {
-            return 'n_enc';
-        }
+        return $resultado;
     }
 
     public static function busca_nome($info) {
-        return 'n_enc';
+        return $info;
     }
 }
--- Projeto/resources/views/busca_medico.blade.php
+++ Projeto/resources/views/busca_medico.blade.php
@@ -5,7 +5,7 @@
 <div>
     <form class="form-block md-form form-sm active-cyan-2 mt-2">
         <div class="form-inline">
-            <input id="campo_busca" class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Digite o nome do(a) médico(a)..." aria-label="Busca" name="busca">
+            <input id="campo_busca" class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Digite o nome do(a) médico(a)..." aria-label="Busca" name="busca" required="required">
             <button class="btn btn-secondary" type="button">
                 <i class="fa fa-search"></i>
         </div>
@@ -26,6 +26,10 @@
     $('#opcoes').on('change', function() {
         var opcao = $(this).find('option:selected').val()
         $('#campo_busca').attr('placeholder', 'Digite o ' + opcao + ' do(a) médico(a)...')
+        if (opcao == 'CRM')
+            $('#campo_busca').attr('type', 'number');
+        else
+            $('#campo_busca').attr('type', 'text');
     })
 
     $(document).attr("title", "Cirurgia - Busca");;
