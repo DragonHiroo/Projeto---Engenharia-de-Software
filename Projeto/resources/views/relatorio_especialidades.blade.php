efcfa24e836526b2eed8af56f594f8edb0cd0dbd
walo2 <42192595+walo2@users.noreply.github.com>
2019-11-17 05:49:45 -0300

    Correções nas views de relatórios

--- Projeto/resources/views/relatorio_especialidades.blade.php
+++ Projeto/resources/views/relatorio_especialidades.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+    $(document).attr("title", "Cirurgia - Relatório");;
+    $('#titulo_topo').text("Relatório: Histórico de pacientes por cirurgia")
+</script>
 @endsection
--- Projeto/resources/views/relatorio_pacientes.blade.php
+++ Projeto/resources/views/relatorio_pacientes.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+    $(document).attr("title", "Cirurgia - Relatório");;
+    $('#titulo_topo').text("Relatório: Histórico de cirurgias por especialiade")
+</script>
 @endsection
